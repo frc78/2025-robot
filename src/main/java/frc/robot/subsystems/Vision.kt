6c4b6fb73865c883ffd0f5a9619a37503f99177b
Tim Winters <tim.winters@bevi.co>
2025-01-20 22:19:11 -0500

    Add comments

--- src/main/java/frc/robot/subsystems/Vision.kt
+++ src/main/java/frc/robot/subsystems/Vision.kt
@@ -1,14 +1,9 @@
 package frc.robot.subsystems
 
-import edu.wpi.first.apriltag.AprilTagFieldLayout
-import edu.wpi.first.apriltag.AprilTagFields
 import edu.wpi.first.math.geometry.Transform3d
-import org.photonvision.EstimatedRobotPose
-import org.photonvision.PhotonCamera
-import org.photonvision.PhotonPoseEstimator
 
 object Vision {
-    val cams: List<Camera> = listOf(
-        Camera("LU", Transform3d()),
-        Camera("RU", Transform3d()))
+    // TW: The naming convention for swerve is F/B for front/back and L/R for left/right
+    // TW: So these would be named FL, FR
+    val cams: List<Camera> = listOf(Camera("LU", Transform3d()), Camera("RU", Transform3d()))
 }
