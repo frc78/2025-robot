b51a6ed981d4250f0552222782364972028afc0f
Coder12121 <levicarter.hubbell@gmail.com>
2025-01-30 13:55:24 -0500

    Add robot states enum and positions for superstructure (#5)
    Co-authored-by: Tim Winters <twinters@aquidneckislandrobotics.org>
    
    Adds an enum of robot states, as well as the positions of the wrist, elevator, and pivot for each of these
    states.
    
    Commands to go to each of these states is aded to smart dashboard for simple control without a joystick

--- src/main/java/frc/robot/subsystems/Wrist.kt
+++ src/main/java/frc/robot/subsystems/Wrist.kt
@@ -1,5 +1,10 @@
 package frc.robot.subsystems
 
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.PrintCommand
 import edu.wpi.first.wpilibj2.command.Subsystem
 
-object Wrist : Subsystem {}
+object Wrist : Subsystem {
+    fun goTo(state: RobotState): Command =
+        PrintCommand("Wrist going to $state - ${state.wristAngle}")
+}
