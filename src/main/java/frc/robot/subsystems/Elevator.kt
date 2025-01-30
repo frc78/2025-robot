a7163237a772fd26a2e1ac107e1bd4fb4d3b8e53
Levi Hubbell <levicarter.hubbell@gmail.com>
2025-01-29 20:19:41 -0500

    Fix

--- src/main/java/frc/robot/subsystems/Elevator.kt
+++ src/main/java/frc/robot/subsystems/Elevator.kt
@@ -1,5 +1,6 @@
 package frc.robot.subsystems
 
+import edu.wpi.first.wpilibj2.command.PrintCommand
 import edu.wpi.first.wpilibj2.command.Subsystem
 
 object Elevator : Subsystem {
--- src/main/java/frc/robot/subsystems/Wrist.kt
+++ src/main/java/frc/robot/subsystems/Wrist.kt
@@ -1,11 +1,8 @@
 package frc.robot.subsystems
 
+import edu.wpi.first.wpilibj2.command.PrintCommand
 import edu.wpi.first.wpilibj2.command.Subsystem
 
-<<<<<<< HEAD
-object Wrist : Subsystem {}
-=======
 object Wrist : Subsystem {
     fun goTo(state: RobotState) = PrintCommand("Wrist going to $state - ${state.wristAngle}")
 }
->>>>>>> 697c837 (RobotoStates)
