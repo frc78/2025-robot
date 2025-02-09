8696d91357b4aaef9d2707757ce5c3b11b521222
Tim Winters <twinters@aquidneckislandrobotics.org>
2025-02-08 19:44:10 -0500

    Spotless and logging

--- src/main/java/frc/robot/subsystems/Intake.kt
+++ src/main/java/frc/robot/subsystems/Intake.kt
@@ -1,32 +1,45 @@
 package frc.robot.subsystems
 
 import com.ctre.phoenix6.hardware.CANrange
+import edu.wpi.first.networktables.NetworkTableInstance
 import edu.wpi.first.wpilibj2.command.Subsystem
 import frc.robot.lib.centimeters
+import org.littletonrobotics.junction.Logger
 
 object Intake : Subsystem {
-    val canRange: CANrange = CANrange(0)
+    private val canRange: CANrange = CANrange(0)
 
-    const val sidePlateThickness = 0.5 // Measured in cm.
-    const val intakeWidth = 52.0 // Measured in cm.
+    private val canRangeOffsetEntry =
+        NetworkTableInstance.getDefault().getEntry("intake/canRangeOffset")
+
+    private val canRangeOffset
+        get() = canRangeOffsetEntry.getDouble(4.0)
+
+    private const val SIDE_PLATE_THICKNESS = 0.5 // Measured in cm.
+    private const val INTAKE_WIDTH = 52.0 // Measured in cm.
 
     val hasCoral: Boolean
-        get() = canRange.getIsDetected().value
+        get() = canRange.isDetected.value
 
     // Returns the distance from the center of the intake to the center of the coral.
-    fun distance(): Double {
+    fun coralLocation(): Double {
         if (!hasCoral) {
             return 0.0
         }
 
-        return rawDistance() - (intakeWidth / 2) + 5.715
+        return rawDistance() - (INTAKE_WIDTH / 2) + 5.715
     }
 
     // Returns the distance from the sensor to the nearest object's edge.
     // Usually returns around +/- 1cm.
-    fun rawDistance(): Double {
-        val original = canRange.getDistance().value.centimeters
+    private fun rawDistance(): Double {
+        val original = canRange.distance.value.centimeters
+        return original - canRangeOffset - SIDE_PLATE_THICKNESS
+    }
 
-        return original - 4.0 - sidePlateThickness
+    override fun periodic() {
+        Logger.recordOutput("intake/coral_detected", hasCoral)
+        Logger.recordOutput("intake/coral_position", rawDistance())
+        Logger.recordOutput("intake/coral_location", coralLocation())
     }
 }
