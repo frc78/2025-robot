db874509b119560c3df19df1f0dcb1cd1fc037ad
Landon Montecalvo <115672301+ljmontecalvo@users.noreply.github.com>
2025-02-13 18:34:19 -0500

    Added elevator test commands.
    Y - Elevator 2v up.
    A - Elevator -2v down.

--- src/main/java/frc/robot/subsystems/Elevator.kt
+++ src/main/java/frc/robot/subsystems/Elevator.kt
@@ -2,7 +2,9 @@ package frc.robot.subsystems
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration
 import com.ctre.phoenix6.controls.DifferentialFollower
+import com.ctre.phoenix6.controls.Follower
 import com.ctre.phoenix6.controls.MotionMagicVoltage
+import com.ctre.phoenix6.controls.VoltageOut
 import com.ctre.phoenix6.hardware.TalonFX
 import com.ctre.phoenix6.signals.GravityTypeValue
 import com.ctre.phoenix6.signals.InvertedValue
@@ -12,9 +14,11 @@ import edu.wpi.first.wpilibj2.command.PrintCommand
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.lib.inches
 import frc.robot.lib.toAngle
+import frc.robot.lib.volts
 
 object Elevator : SubsystemBase("Elevator") {
     val motionMagic = MotionMagicVoltage(0.0)
+    val voltage = VoltageOut(0.0)
 
     fun goTo(state: RobotState): Command =
         PrintCommand("Elevator going to $state - ${state.elevatorHeight}")
@@ -26,6 +30,14 @@ object Elevator : SubsystemBase("Elevator") {
                     )
                 }
             )
+
+    fun manualUp() : Command {
+        return startEnd({ leader.setControl(voltage.withOutput(2.0.volts)) }, { leader.setControl(voltage.withOutput(0.0.volts)) })
+    }
+
+    fun manualDown() : Command {
+        return startEnd({ leader.setControl(voltage.withOutput(-2.0.volts)) }, { leader.setControl(voltage.withOutput(0.0.volts)) })
+    }
 
     var height = 0.inches
 
@@ -82,6 +94,6 @@ object Elevator : SubsystemBase("Elevator") {
 
     val follower =
         TalonFX(FOLLOWER_MOTOR_ID, "*").apply {
-            setControl(DifferentialFollower(LEADER_MOTOR_ID, true))
+            setControl(Follower(LEADER_MOTOR_ID, true))
         }
 }
