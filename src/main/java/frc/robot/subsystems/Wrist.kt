d8958d2a9fa8a295dea04e3b52c3b55166ed69a2
Timothy Sieben <tsieben@aquidneckislandrobotics.org>
2025-02-16 16:46:56 -0500

    Changed import

--- src/main/java/frc/robot/subsystems/Wrist.kt
+++ src/main/java/frc/robot/subsystems/Wrist.kt
@@ -1,54 +1,127 @@
 package frc.robot.subsystems
 
+import com.ctre.phoenix6.SignalLogger
 import com.ctre.phoenix6.configs.MotorOutputConfigs
+import com.ctre.phoenix6.configs.TalonFXConfiguration
+import com.ctre.phoenix6.controls.MotionMagicVoltage
 import com.ctre.phoenix6.controls.VoltageOut
 import com.ctre.phoenix6.hardware.TalonFX
-import com.ctre.phoenix6.signals.NeutralModeValue
-import edu.wpi.first.wpilibj2.command.Command
-import edu.wpi.first.wpilibj2.command.PrintCommand
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
-import edu.wpi.first.wpilibj2.command.SubsystemBase
-import frc.robot.lib.amps
-import frc.robot.lib.degrees
-import frc.robot.lib.volts
+import com.ctre.phoenix6.signals.*
+import edu.wpi.first.units.measure.Angle
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
+import edu.wpi.first.wpilibj2.command.*
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine
+import frc.robot.lib.*
 
 object Wrist : SubsystemBase("Wrist") {
-    val leader =
+    private var lowerLimit = 0.degrees
+    private var upperLimit = 100.degrees
+    private const val MOTOR_TO_WRIST = 138.857
+
+    private val leader =
         TalonFX(13, "*").apply {
-            configurator.apply(MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake))
+            val config = TalonFXConfiguration().apply {
+                Feedback.SensorToMechanismRatio = MOTOR_TO_WRIST
+                MotorOutput.Inverted = InvertedValue.Clockwise_Positive
+                MotorOutput.NeutralMode = NeutralModeValue.Brake
+
+                SoftwareLimitSwitch.withForwardSoftLimitEnable(true)
+                    .withReverseSoftLimitEnable(true)
+                    .withForwardSoftLimitThreshold(upperLimit)
+                    .withReverseSoftLimitThreshold(lowerLimit)
+
+                Slot0.withKP(0.0)
+                    .withKD(0.0)
+                    .withKS(0.0)
+                    .withKV(2.73)
+                    .withKA(0.02)
+                    .withKG(0.31)
+                    .withGravityType(GravityTypeValue.Arm_Cosine)
+
+                MotionMagic.MotionMagicCruiseVelocity = .25
+                MotionMagic.MotionMagicAcceleration = 2.5
+            }
+
+            configurator.apply (config)
         }
-    var lowerLimit = 0.degrees
-    var upperLimit = 0.degrees
+
+    val motionMagic = MotionMagicVoltage(0.degrees)
+    val voltageOut = VoltageOut(0.0)
 
     fun goTo(state: RobotState): Command =
         PrintCommand("Wrist going to $state - ${state.wristAngle}")
-            .alongWith(runOnce { angle = state.wristAngle })
+            .alongWith(runOnce { leader.setControl(motionMagic.withPosition(state.wristAngle.wristToMotor())) })
 
-    var angle = 0.degrees
-    val voltage = VoltageOut(0.0)
+    val angle: Angle
+        get() = leader.position.value
 
     fun manualUp(): Command {
         return startEnd(
-            { leader.setControl(voltage.withOutput(2.0.volts)) },
-            { leader.setControl(voltage.withOutput(0.0.volts)) },
+            { leader.setControl(voltageOut.withOutput(2.0.volts)) },
+            { leader.setControl(voltageOut.withOutput(0.0.volts)) },
         )
     }
 
     fun manualDown(): Command {
         return startEnd(
-            { leader.setControl(voltage.withOutput(-2.0.volts)) },
-            { leader.setControl(voltage.withOutput(0.0.volts)) },
+            { leader.setControl(voltageOut.withOutput((-2.0).volts)) },
+            { leader.setControl(voltageOut.withOutput(0.0.volts)) },
         )
     }
 
     fun zeroPosition(): Command {
         return SequentialCommandGroup(
-            manualUp()
-                .until({ leader.torqueCurrent.value > 10.0.amps })
-                .andThen({ upperLimit = leader.position.value }),
             manualDown()
                 .until({ leader.torqueCurrent.value > 10.0.amps })
-                .andThen({ lowerLimit = leader.position.value }),
+                .andThen({ leader.setPosition(0.0.degrees)
+                    lowerLimit = leader.position.value + 5.degrees
+                }),
+            manualUp()
+                .until({ leader.torqueCurrent.value > 10.0.amps })
+                .andThen({ upperLimit = leader.position.value - 5.degrees }),
         )
     }
+
+    private val sysIdRoutine =
+        SysIdRoutine(
+            SysIdRoutine.Config(
+                .3.voltsPerSecond,
+                3.volts,
+                10.seconds,
+                { SignalLogger.writeString("wrist_state", "$it") },
+            ),
+            SysIdRoutine.Mechanism(
+                { leader.setControl(voltageOut.withOutput(it)) },
+                null,
+                this,
+                "wrist",
+            ),
+        )
+
+    private val sysId =
+        Commands.sequence(
+            runOnce { SignalLogger.start() },
+            sysIdRoutine.quasistatic(SysIdRoutine.Direction.kForward).until {
+                leader.position.value >= upperLimit
+            },
+            sysIdRoutine.quasistatic(SysIdRoutine.Direction.kReverse).until {
+                leader.position.value <= lowerLimit
+            },
+            sysIdRoutine.dynamic(SysIdRoutine.Direction.kForward).until {
+                leader.position.value >= upperLimit
+            },
+            sysIdRoutine.dynamic(SysIdRoutine.Direction.kReverse).until {
+                leader.position.value <= lowerLimit
+            },
+            runOnce { SignalLogger.stop() },
+        )
+            .withName("Pivot SysId")
+
+    init {
+        SmartDashboard.putData(this)
+        SmartDashboard.putData(sysId)
+    }
+
+    fun Angle.motorToWrist() = this / MOTOR_TO_WRIST
+    fun Angle.wristToMotor() = this * MOTOR_TO_WRIST
 }
