f974ad3b1a1b9f983faac5d0f45d8fea8dfee41e
Tim Winters <twinters@aquidneckislandrobotics.org>
2025-02-21 12:29:47 -0500

    Spotless

--- src/main/java/frc/robot/lib/Alignments.kt
+++ src/main/java/frc/robot/lib/Alignments.kt
@@ -12,7 +12,10 @@ import kotlin.jvm.optionals.getOrNull
 
 /** Poses that the robot can auto-align to */
 object Alignments {
-    private val aprilTags = AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeAndyMark) // TODO NEED TO KEEP EYE ON
+    private val aprilTags =
+        AprilTagFieldLayout.loadField(
+            AprilTagFields.k2025ReefscapeAndyMark
+        ) // TODO NEED TO KEEP EYE ON
 
     val REEF_TO_BRANCH_LEFT = Transform2d(0.meters, -(13 / 2).inches, Rotation2d.kZero)
     val REEF_TO_BRANCH_RIGHT = Transform2d(0.meters, (13 / 2).inches, Rotation2d.kZero)
