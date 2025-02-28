8c45a701ae3fae771d6f8e70dd8aba29679a9f54
Nehuel Armenanzas <74217788+uncookednehuel@users.noreply.github.com>
2025-02-27 20:06:15 -0500

    Implemented SDF

--- src/main/java/frc/robot/lib/FieldObstacles.kt
+++ src/main/java/frc/robot/lib/FieldObstacles.kt
@@ -1,6 +1,9 @@
 package frc.robot.lib
 
+import edu.wpi.first.math.MathUtil
+import edu.wpi.first.math.Vector
 import edu.wpi.first.math.geometry.Translation2d
+import edu.wpi.first.math.numbers.N2
 import edu.wpi.first.wpilibj.DriverStation
 import frc.robot.subsystems.Intake
 
@@ -18,15 +21,9 @@ object FieldObstacles {
 
     val CORAL_STATIONS =
         if (DriverStation.getAlliance().equals(DriverStation.Alliance.Blue)) {
-            listOf(
-                BLUE_CORAL_STATION_LEFT,
-                BLUE_CORAL_STATION_RIGHT,
-            )
+            listOf(BLUE_CORAL_STATION_LEFT, BLUE_CORAL_STATION_RIGHT)
         } else {
-            listOf(
-                RED_CORAL_STATION_LEFT,
-                RED_CORAL_STATION_RIGHT,
-            )
+            listOf(RED_CORAL_STATION_LEFT, RED_CORAL_STATION_RIGHT)
         }
 }
 
@@ -36,8 +33,13 @@ abstract class Obstacle() {
 
 class LineSegment(val p1: Translation2d, val p2: Translation2d, val applySlow: () -> Boolean) :
     Obstacle() {
-    private fun getShortestDistance(): Double {
-        TODO("Not yet implemented. SDFs!!!! Tree math!! (Basic tree math)")
+    private fun getShortestDistance(position: Translation2d): Double {
+        // Source: Inigo Quilez https://iquilezles.org/articles/distfunctions2d/
+        val positionToP1: Vector<N2> = position.toVector().minus(p1.toVector())
+        val p2ToP1: Vector<N2> = p2.toVector().minus(p1.toVector())
+
+        val h = MathUtil.clamp(positionToP1.dot(p2ToP1) / p2ToP1.dot(p2ToP1), 0.0, 1.0)
+        return positionToP1.minus(p2ToP1.times(h)).norm()
     }
 
     override fun getSlowdownFactor(): Double {
@@ -46,4 +48,18 @@ class LineSegment(val p1: Translation2d, val p2: Translation2d, val applySlow: (
         }
         return 1.0
     }
+}
+
+// In progress
+private fun quadraticDecrease(
+    distance: Double,
+    startSlowDistance: Double,
+    maxSlowDistance: Double,
+    maxSlowdown: Double,
+): Double {
+    return MathUtil.clamp(
+        distance - startSlowDistance,
+        0.0,
+        maxSlowDistance,
+    )
 }
