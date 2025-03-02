11cd947a5a1081754afa17f514b167cc010b6a87
Levi Hubbell <levicarter.hubbell@gmail.com>
2025-03-02 16:31:07 -0500

    Auto Commands

--- src/main/java/frc/robot/auto/Autos.kt
+++ src/main/java/frc/robot/auto/Autos.kt
@@ -5,17 +5,40 @@ import com.pathplanner.lib.path.PathPlannerPath
 import edu.wpi.first.wpilibj2.command.Commands
 import frc.robot.lib.Alignments
 import frc.robot.lib.command
+import frc.robot.subsystems.Intake
+import frc.robot.subsystems.RobotState
+import frc.robot.subsystems.SuperStructure
 import frc.robot.subsystems.drivetrain.Chassis
 
 object Autos {
     val FourCoralAuto by command {
-        val lineToIJ = PathPlannerPath.fromPathFile("AutoLine to IJ")
+        val lineToIJ = PathPlannerPath.fromPathFile("AutoLine to IJ").mirrorPath()
         val ijToCoral = PathPlannerPath.fromPathFile("IJToCoral")
         Commands.sequence(
             AutoBuilder.followPath(lineToIJ),
-            AutoBuilder.followPath(ijToCoral),
-            Chassis.driveToPose { Alignments.closestCoralStation },
-            Chassis.driveToPose { Alignments.ReefFace.KL.pose },
+            Chassis.driveToRightBranch,
+            SuperStructure.smartGoTo(RobotState.L4),
+            Intake.scoreCoral,
+            SuperStructure.smartGoTo(RobotState.CoralStation),
+            Chassis.driveToClosestCoralStation,
+            (Intake.intakeCoralThenHold()),
+            (SuperStructure.smartGoTo(RobotState.PreScore)).alongWith(Chassis.driveToLeftBranch),
+            SuperStructure.smartGoTo(RobotState.L4),
+            Intake.scoreCoral,
+            SuperStructure.smartGoTo(RobotState.CoralStation),
+            Chassis.driveToClosestCoralStation,
+            (Intake.intakeCoralThenHold()),
+            (SuperStructure.smartGoTo(RobotState.PreScore)).alongWith(Chassis.driveToRightBranch),
+            SuperStructure.smartGoTo(RobotState.L4),
+            Intake.scoreCoral,
+            SuperStructure.smartGoTo(RobotState.CoralStation),
+            Chassis.driveToClosestCoralStation,
+            Intake.intakeCoralThenHold(),
+            (Intake.intakeCoralThenHold()),
+            (SuperStructure.smartGoTo(RobotState.PreScore)).alongWith(Chassis.driveToPose { Alignments.ReefFace.AB.pose }),
+            Chassis.driveToLeftBranch,
+            SuperStructure.smartGoTo(RobotState.L4),
+            Intake.scoreCoral,
         )
     }
 }
