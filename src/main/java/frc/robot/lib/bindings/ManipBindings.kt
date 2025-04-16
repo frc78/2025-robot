ba8c6d412bde96f74aeb6b3602f9bfc98aeb2dee
bigrabbit555 <sdell624@gmail.com>
2025-04-16 09:49:05 -0400

    fixed wildcard imports

--- src/main/java/frc/robot/lib/bindings/ManipBindings.kt
+++ src/main/java/frc/robot/lib/bindings/ManipBindings.kt
@@ -4,20 +4,20 @@ import edu.wpi.first.wpilibj.XboxController
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController
-import frc.robot.lib.inches
-import frc.robot.lib.andWait
+import frc.robot.lib.Branch
 import frc.robot.lib.Level
+import frc.robot.lib.ScoreSelector
 import frc.robot.lib.ScoreSelector.SelectedBranch
+import frc.robot.lib.andWait
+import frc.robot.lib.inches
 import frc.robot.subsystems.Climber
 import frc.robot.subsystems.Elevator
-import frc.robot.lib.ScoreSelector
 import frc.robot.subsystems.Intake
 import frc.robot.subsystems.Pivot
 import frc.robot.subsystems.RobotState
 import frc.robot.subsystems.SuperStructure
 import frc.robot.subsystems.Wrist
 import frc.robot.subsystems.drivetrain.Chassis
-import frc.robot.lib.Branch
 import kotlin.math.absoluteValue
 import org.littletonrobotics.junction.Logger
 
