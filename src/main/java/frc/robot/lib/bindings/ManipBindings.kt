fb9a5d22e16bbc2801358442a0adaa8863b3a91d
bigrabbit555 <sdell624@gmail.com>
2025-04-16 09:59:42 -0400

    spotless again... yippee!!!!

--- src/main/java/frc/robot/lib/bindings/ManipBindings.kt
+++ src/main/java/frc/robot/lib/bindings/ManipBindings.kt
@@ -1,7 +1,6 @@
 package frc.robot.lib.bindings
 
 import edu.wpi.first.wpilibj.XboxController
-import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController
