14a743637febd8a469c97e3ea3686b518a123fcc
bigrabbit555 <sdell624@gmail.com>
2025-06-08 09:08:45 -0400

    slight (0.25 degree) reduction to coral station pivot angle after it being too high in a few matches

--- src/main/java/frc/robot/subsystems/SuperStructure.kt
+++ src/main/java/frc/robot/subsystems/SuperStructure.kt
@@ -31,7 +31,7 @@ enum class RobotState(val pivotAngle: Angle, val elevatorHeight: Distance, val w
     L3(92.6.degrees, 17.25.inches, 38.degrees),
     L4(90.5.degrees, 47.inches, 29.25.degrees), // 92.5 pivot
     CoralStation(62.92.degrees, 0.25.inches, 176.degrees),
-    NewCoralStation(67.1.degrees, 0.1.inches, 185.8.degrees), // pivot 68.2
+    NewCoralStation(66.85.degrees, 0.1.inches, 185.8.degrees), // pivot 68.2 // 67.1 pivot 6/8
     AlgaeGroundPickup(26.degrees, 0.25.inches, 181.35.degrees),
     Processor(10.7.degrees, 0.25.inches, 86.4.degrees),
     HighAlgaeIntake(100.2.degrees, 19.33.inches, 13.25.degrees),
